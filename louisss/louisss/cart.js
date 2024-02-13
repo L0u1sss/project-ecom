ffefc2f477b56d5ab7e991103393f212625fa8c3
L0u1sss <phalat.lui@gmail.com>
2024-02-13 23:42:05 +0700

    131321

---
+++ louisss/louisss/cart.js
@@ -0,0 +1,11 @@
+console.log('connect cart.js');
+var app = angular.module('myAppCart', []);
+app.controller('myCtrlCart', function ($scope) {
+    console.log('connect cart.js controller');
+
+  info();
+  function info() {
+
+  }
+
+});

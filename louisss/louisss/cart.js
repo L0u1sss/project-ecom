b5a047b8907d070d2571e95df5ae38e06c958b3c
L0u1sss <phalat.lui@gmail.com>
2024-02-14 22:24:27 +0700

    cart

--- louisss/louisss/cart.js
+++ louisss/louisss/cart.js
@@ -1,11 +1,38 @@
 console.log('connect cart.js');
 var app = angular.module('myAppCart', []);
 app.controller('myCtrlCart', function ($scope) {
-    console.log('connect cart.js controller');
+  console.log('connect cart.js controller');
 
   info();
   function info() {
+    var settings = {
+      "url": "http://localhost/php-api/index.php/api/User3/GetBookInCart",
+      "method": "POST",
+      "timeout": 0,
+      "headers": {
+        "X-Access-Token": localStorage.getItem("token")
+      },
+    };
 
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      if (response) {
+        let item = response;
+        console.log(item);
+        let k = response.Item;
+        console.log(k);
+        $scope.k = k;
+        console.log($scope.k);
+        $scope.name = item.Item[0].Fname;
+        $scope.sumamount = item.amount
+        $scope.sumprince = item.prince
+        console.log($scope.sumprince);
+        // $scope.x = item.Item.Fname;
+        // console.log($scope.item);
+        // $scope.image = "http://localhost/" + item.Item.image;
+        $scope.$apply();
+      }
+    });
   }
 
 });
